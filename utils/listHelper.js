3bf33da507f75f950ff8da945ca24fb88e39d0e9
maaush <mauribenitez95@gmail.com>
2020-05-30 23:23:03 -0300

    Finish exercise 4.5

--- utils/listHelper.js
+++ utils/listHelper.js
@@ -8,8 +8,13 @@ const totalLikes = (blogs) => {
   return blogs.reduce((total, blog) => total + Number(blog.likes), 0);
 }
 
+const favoriteBlog = (blogs) => {
+
+  return blogs.reduce((mostLikedBlog, blog) => mostLikedBlog.likes >= blog.likes ? mostLikedBlog : blog);
+}
+
 module.exports = {
   dummy,
   totalLikes,
-
+  favoriteBlog
 }
