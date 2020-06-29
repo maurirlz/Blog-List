40a5135d868276403a5cbada5495f3304861ef37
maaush <mauribenitez95@gmail.com>
2020-06-29 17:55:17 -0300

    Add testing router for resetting the database in order to start fresh E2E tests

--- app.js
+++ app.js
@@ -30,6 +30,11 @@ app.use(express.json());
 app.use(middleware.requestLogger);
 app.use(middleware.tokenExtractor);
 
+if (process.env.NODE_ENV === 'test') {
+  const testingRouter = require('./controllers/testing');
+  app.use('/api/testing', testingRouter);
+}
+
 app.use('/api/blogs', blogRouter);
 app.use('/api/users', userRouter);
 app.use('/api/login', loginRouter);
